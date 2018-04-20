b55b37925671d3ce4e2410d8797ba3a8553fb39e
PeculiarVegetation <bwhipkey@mail.umw.edu>
2018-04-19 21:08:40 -0400

    Manually uploaded files because reasons
    THESE HAVE NOT BEEN TESTED. I REPEAT, THESE HAVE NOT BEEN TESTED.

--- src/Memory.java
+++ src/Memory.java
@@ -1,42 +1,41 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.util.Scanner;
 
 public class Memory {
 
+    private Controller controller;
+
     private int size;
-    //private String[] contents;  //Make tuple - DONE; Address class
-    private Address[] contents;
+    private String[] contents;
     private int length;
 
-    public Memory(int size)
+    public Memory(int size, Controller controller)
     {
         this.size = size;
-        //this.contents = new String[this.size];
-        this.contents = new Address[this.size];
-//        for (String s: this.contents)
-//        {
-//            s = "00000000";
-//        }
+        this.contents = new String[this.size];
         for (int i = 0; i < this.size; i++)
         {
-            this.contents[i] = new Address(false, "", 0);
+            this.contents[i] = "00000000";
         }
         this.length = 0;
+        this.controller = controller;
     }
 
-    public Memory(int size, File infile) throws FileNotFoundException
+    public Memory(int size, Controller controller, File infile) throws FileNotFoundException, IllegalArgumentException
     {
+        if(size < 64 || size > 8192)
+        {
+            throw new IllegalArgumentException("Error: illegal memory size!");
+        }
         this.size = size;
-        this.contents = new Address[this.size];
+        this.contents = new String[this.size];
         for (int i = 0; i < this.size; i++)
         {
-            this.contents[i] = new Address(false, "", 0);
+            this.contents[i] = "00000000";
         }
-        //InputStream in = new java.io.FileInputStream(infile);
-        parse(infile);
-
+        readFile(infile);
+        this.controller = controller;
     }
 
     public void setSize(int size)
@@ -55,7 +54,7 @@ public class Memory {
     }
 
 
-    public boolean write(int addr, int value)
+    public boolean write(int addr, String value)
     {
         //Need check for valid value length
         if(addr < 0 || addr >= this.size)
@@ -64,42 +63,87 @@ public class Memory {
         }
         else
         {
-            this.contents[addr].value = value;  //Need to parse to determine Address
+            if(value.length() != 8)  //All values must be 8 bits long, OR ELSE!
+            {
+                return(false);
+            }
+            this.contents[addr] = value;
+            return(true);
+        }
+    }
+    
+    public boolean writeByte(int addr, String value)
+    {
+        //Need check for valid value length
+        if(addr < 0 || addr + 3 >= this.size)
+        {
+            return(false);
+        }
+        else
+        {
+            if(value.length() != 32)  //All values must be 32 bits long, OR ELSE!
+            {
+                return(false);
+            }
+            for(int i = 0; i < 4; i++)
+            {
+                this.contents[addr + i] = value.substring(i * 8, (i + 1) * 8);
+            }
             return(true);
         }
     }
 
-    public Address read(int addr)
+    /*DEPRECATED, here for reference purposes*/
+//    public boolean writeCommand(int addr, String command)
+//    {
+//        //Need check for valid value length
+//        if(addr < 0 || addr >= this.size)
+//        {
+//            return(false);
+//        }
+//        else
+//        {
+//            this.contents[addr].operation = command;
+//            this.contents[addr].instruction = true;
+//            return(true);
+//        }
+//    }
+
+    public String read(int addr) throws Exception
     {
         if(addr < 0 || addr >= this.size)
         {
-            return(null);
+            throw new Exception("Error: attempted to access memory out of bounds");
         }
-        else
-        {
-            return(this.contents[addr]);
-        }
+
+        return(this.contents[addr]);
     }
 
-    private void parse(File infile) throws FileNotFoundException
+    private void readFile(File infile) throws FileNotFoundException
     {
         Scanner file_reader = new Scanner(infile);
-        String temp;
+        String line;
         int addr = 0;
 
         while(file_reader.hasNextLine())
         {
-            temp = file_reader.nextLine();
+            line = file_reader.nextLine();
 
-            //Code to break up each line goes here
-
-            //Code to translate between ARM assembly/hex/binary goes here
-
-            contents[addr].operation = temp;
+            this.contents[addr] = line;
             addr++;
         }
 
         this.length = addr;
+    }
+
+    public void reset()
+    {
+        for(int i = 0; i < this.length; i++)
+        {
+            this.contents[i] = "00000000";
+        }
+
+        this.length = 0;
     }
 
 }
---
+++ src/Utils.java
@@ -0,0 +1,53 @@
+public class Utils
+{
+    public static final int WORD_SIZE = 32;
+    public static final int BITS_PER_ADDR = 8;
+    public static final int ADDRS_PER_WORD = WORD_SIZE / BITS_PER_ADDR;
+
+    public static final int PC = 15;
+    public static final int SP = 13;
+    public static final int LR = 14;
+
+    public enum Operation
+    {
+        ADD, SUB, LDR, STR
+    }
+
+    public enum Condition
+    {
+        EQ, NE, CS, CC, MI, PL, VS, VC, HI, LS, GE, LT, GT, LE, AL, UNCONDITIONAL_INSTRUCTION
+    }
+
+    public enum Processing
+    {
+        AND, EOR, SUB, RSB, ADD, ADC, SBC, RSC, TST, TEQ, CMP, CMN, ORR, MOV, BIC, MVN
+    }
+
+    public enum Shift
+    {
+        LSL /*00*/, LSR /*01*/, ASR /*10*/, ROR /*11*/, RRX /*11*/
+    }
+    
+    public enum Instruction
+    {
+        DATA_PROC, DATA_PROC_IMM, LOAD_STORE_IMM_OFFSET, LOAD_STORE_REG_OFFSET, LOAD_STORE_MULT, BRANCH, CO_LOAD_STORE, CO_DATA_PROC
+    }
+    
+    public enum Branch
+    {
+        B, BL, BLX, BX, BXJ
+    }
+    
+    public enum Load_Store
+    {
+        LDR, LDRB, LDRBT, LDRD, LDREX, LDRH, LDRSB, LDRSH, LDRT, STR, STRB, STRBT, STRD, STREX, STRH, STRT
+    }
+
+    public static void printIf(String s, boolean toPrint)
+    {
+        if(toPrint)
+        {
+            System.out.print(s);
+        }
+    }
+}
